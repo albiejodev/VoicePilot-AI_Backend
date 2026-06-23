14ea9e6a677fb470eb25e1a5eff8985b3bd18afc
Albiejo <albiejosephs101@gmail.com>
2026-06-23 13:13:23 +0530

    prepare for deepgram integration

--- app/core/config.py
+++ app/core/config.py
@@ -15,6 +15,8 @@ class Settings(BaseSettings):
 
     LOG_LEVEL: str = "INFO"
 
+    DEEPGRAM_API_KEY: str
+
     class Config:
         env_file = ".env"
 
---
+++ app/services/stt/deepgram_service.py
@@ -0,0 +1,9 @@
+class DeepgramService:
+    async def connect(self):
+        pass
+
+    async def send_audio(self, chunk):
+        pass
+
+    async def disconnect(self):
+        pass
